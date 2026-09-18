e1d4e63f277feb96421ca152921943425567d73a
Danielherrada1228 <dherrada@unal.edu.co>
2026-09-17 21:42:02 -0500

    Add Calcular class for area and circumference calculations

---
+++ Ej_17/Calcular.java
@@ -0,0 +1,11 @@
+public class Calcular {
+    public static double A(double r) {
+    return Math.PI*Math.pow(r, 2);
+    }
+    
+    public static double L(double r){
+    return 2*r*Math.PI;
+    }
+    
+    
+}
