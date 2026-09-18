d8bc2158e2401a6e1fee8a412146a5c4978571bd
Danielherrada1228 <dherrada@unal.edu.co>
2026-09-17 21:40:44 -0500

    Add Calcular class with square and cube methods

---
+++ Ej_14/Calcular.java
@@ -0,0 +1,11 @@
+public class Calcular {
+    public static double cuadrado(double x) {
+    return Math.pow(x,2);
+    }
+    
+    public static double cubo(double x){
+    return Math.pow(x,3);
+    }
+    
+    
+}
